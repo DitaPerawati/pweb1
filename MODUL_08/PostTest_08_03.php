e7d843b1be30bfae804a1e95202dfd4d174c132e
Dita Perawati <20230910025@uniku.ac.id>
2025-01-06 13:46:28 +0700

    menambahkan modul 8

---
+++ MODUL_08/PostTest_08_03.php
@@ -0,0 +1,10 @@
+<?php
+//Mendeklarasikan variabel
+$a = 5;
+$b = 10;
+//Operasi Penjumlahan
+$hasil = $a + $b;
+
+//Mencetak hasil dalam bentuk string
+echo "Hasil dari penjumlahan $a + $b adalah $hasil";
+?>
